d4952d01fa4eef266d9381b9868f7c6a99794724
Uma geethika7 <geethikareddy2507@gmail.com>
2025-06-02 11:19:46 +0530

    Add files via upload

---
+++ ListusingArrayListLinkedllistHashsetHashMap.java
@@ -0,0 +1,16 @@
+package xyz;
+import java.util.ArrayList;
+import java.util.LinkedList;
+public class ListusingArrayListLinkedllistHashsetHashMap {
+	public static void main(String[] args) {
+		ArrayList<Integer> ob = new ArrayList<Integer>();
+		LinkedList<Integer> obj = new LinkedList<Integer>();
+		ob.add(20);
+		ob.add(30);
+		obj.add(40);
+		obj.add(50);
+		System.out.println(ob);
+		System.out.println(obj);
+	}
+
+}
