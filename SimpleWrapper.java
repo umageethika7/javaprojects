b9f71ac071de474f94fea597ff0be19b65c80473
Uma geethika7 <geethikareddy2507@gmail.com>
2025-06-02 10:38:19 +0530

    Add files via upload

---
+++ SimpleWrapper.java
@@ -0,0 +1,15 @@
+package xyz;
+
+public class SimpleWrapper {
+
+	public static void main(String[] args) {
+		int a = 1;
+		Integer b = 1;
+		double  price = 99.99;
+		Double price2 = 99.99;
+		System.out.println(price);
+		System.out.println(b);
+
+	}
+
+}
