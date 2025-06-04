9e0367098b2cdffdee36f19028e9d80d51135bf6
Uma geethika7 <geethikareddy2507@gmail.com>
2025-06-04 10:23:55 +0530

    Add files via upload

---
+++ StackeExample.java
@@ -0,0 +1,22 @@
+package uma;
+import java.util.Stack;
+public class StackeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<String> se = new Stack<String>();
+		se.push("plate 1");
+		se.push("plate 2");
+		se.push("plate 3");
+		se.push("plate 4");
+		se.push("plate 5");
+		System.out.println(se);
+		System.out.println(se.pop());
+		System.out.println(se);
+		
+		
+		
+
+	}
+
+}
